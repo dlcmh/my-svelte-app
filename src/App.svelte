061b1269dec5eac61cada9c828aba96c894ecb7b
David Chin <dlcmhd@me.com>
2020-08-17 19:57:37 +0800

    Reacting to DOM Events

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,34 @@
 <script>
   import Navbar from './sections/navbar.svelte';
+
+  let disabled = true;
+  let name = '';
+  let names = [];
+
+  function keyPressed() {
+    disabled = false;
+  }
+
+  function resetClicked() {
+    disabled = true;
+    names = [name, ...names];
+    name = '';
+  }
 </script>
 
 <Navbar title="vCard Generator" />
+
+<form>
+  <label>
+    Name
+    <input
+      type="text"
+      placeholder="Please enter your name"
+      bind:value={name}
+      on:keydown={keyPressed} />
+  </label>
+
+  <button {disabled} on:click|preventDefault={resetClicked}>Reset</button>
+</form>
+
+<p>{names}</p>
